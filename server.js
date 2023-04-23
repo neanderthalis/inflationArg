e217b73b7195c4cac9e143e7eccb81d3b8d8caed
neanderthalis <neanderthalis@proton.me>
2023-04-23 15:31:10 -0300

    set cronjob

--- server.js
+++ server.js
@@ -6,11 +6,23 @@ const processInflationData = require("./dataCollection/processInflationData")
 const getMonthlyInflation = require("./dataCollection/getMonthlyInflation")
 const getCurrentYearInflation = require("./dataCollection/getCurrentYearInflation")
 const cors = require('cors');
-const DATA = {}
+const cron = require("node-cron")
+let DATA = {}
 
 app.use(cors({
   origin: '*'
 }));
+
+const setData = async () => {
+  const inflationTable = await fetchInflationTable()
+  const processedInflationData = processInflationData(inflationTable)
+  DATA.monthlyInflation = getMonthlyInflation(processedInflationData)
+  DATA.currentYearInflation = getCurrentYearInflation(processedInflationData)
+}
+
+cron.schedule('0 12-20 14-17 * *', () => {
+  setData()
+});
 
 app.get('/up', function (req, res) {
   res.send('ok');
@@ -21,9 +33,11 @@ app.get('/2023', (req, res) => {
 })
 
 app.listen(3000, async () => {
-  const inflationTable = await fetchInflationTable()
-  const processedInflationData = processInflationData(inflationTable)
-  DATA.monthlyInflation = getMonthlyInflation(processedInflationData)
-  DATA.currentYearInflation = getCurrentYearInflation(processedInflationData)
-  console.log('ready');
+  try {
+    await setData()
+    console.log('ready');
+  } catch (e) {
+    console.log("Error fetching data")
+  }
+
 });
